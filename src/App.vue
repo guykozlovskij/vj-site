1553717326f594deacd28a9bf37ea01a427ce53c
Guy Kozlovskij <gajus.kozlovskij@gmail.com>
2026-07-07 08:51:35 +0300

    add initial files and boilerplate

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
+<script setup>
+import Contact from './components/layout/Contact.vue';
+</script>
+
 <template>
 
   <main>
     <h1>Hello </h1>
   </main>
+  <Contact />
 
 </template>
 
-<style lang="scss">
+<style scoped lang="scss">
 h1 {
   color: $primary-color;
 }
---
+++ src/components/layout/Contact.vue
@@ -0,0 +1,12 @@
+<template>
+    <h1>
+        Contact Here
+    </h1>
+</template>
+
+<style scoped lang="scss">
+h1 {
+    color: $secondary-color;
+    font-size: 80px;
+}
+</style>
