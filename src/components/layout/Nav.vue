8c1a1d7b9134ec5296baa77df158e90c36029a96
Guy Kozlovskij <gajus.kozlovskij@gmail.com>
2026-07-17 15:04:47 +0200

    nav styling

--- src/components/layout/Nav.vue
+++ src/components/layout/Nav.vue
@@ -5,20 +5,40 @@ const { currentLanguage, setLanguage } = useLanguage()
 
 <template>
   <nav>
-    <button :class="{ active: currentLanguage === 'lt' }" @click="setLanguage('lt')">
-      LT
-    </button>
-    <button :class="{ active: currentLanguage === 'en' }" @click="setLanguage('en')">
-      EN
-    </button>
+    <p style="color:white; font-size: 20px;">VJ</p>
+    <div class="language-selector">
+      <button :class="{ active: currentLanguage === 'lt' }" @click="setLanguage('lt')">
+        🇱🇹
+      </button>
+      <button :class="{ active: currentLanguage === 'en' }" @click="setLanguage('en')">
+        🇬🇧
+      </button>
+    </div>
   </nav>
 </template>
 
 <style scoped lang="scss">
 nav {
-  background-color: rgba(175, 175, 175, 0.575);
+  background-color: rgb(44, 99, 86);
   position: fixed;
+  height: $nav-height;
   top: 0;
   width: 100%;
+  padding: 0 15px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  .language-selector {
+    margin-left: auto;
+    display: flex;
+    gap: 10px;
+    button {
+      font-size: 35px;
+      background: transparent;
+      border: none;
+      padding: 0;
+      cursor: pointer;
+    }
+  }
 }
 </style>
